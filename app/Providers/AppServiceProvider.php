df40c1490934ebf92e16099391180c35175ea6f7
basilgass <basil.gass@bluewin.ch>
2021-09-24 15:32:30 +0200

    Mise à jour pour 1ère production ?

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(App::environment('production')){
+            URL::forceScheme('https');
+        }
+        
         Blade::directive('markdown', function ($expression) {
             // TODO: make markdown work on the website !!!!!
             return "<?php echo $expression; ?>";
